e7bde833253ab98c6163c571662302bdcba23fdf
Una Liu <144079763+aaaaaaaanO@users.noreply.github.com>
2024-12-05 15:24:33 -0500

    Add files via upload

--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import oneDCanvasComponent from "@/components/Canvas/oneDCanvasComponent_5.vue";
-import twoDCanvasComponent from "@/components/Canvas/twoDCanvasComponent_3.vue";
+import oneDCanvasComponent from "@/components/Canvas/oneDCanvasComponent_6.vue";
+import twoDCanvasComponent from "@/components/Canvas/twoDCanvasComponent_4.vue";
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
 import { computed, onMounted, ref } from "vue";
